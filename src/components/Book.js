14a2874feaa37c414cc9ff7b1f72e2d83b9ef5c4
Sami Ullah <samiullahk997@gmail.com>
2023-05-20 16:59:43 +0500

    Update Book.js

--- src/components/Book.js
+++ src/components/Book.js
@@ -42,6 +42,7 @@ export default function Book(props) {
           </div>
         </div>
       ))}
-<!--Begin: Star-Clicks.com HTML Code--><script type='text/javascript' src='https://www.star-clicks.com/secure/ads.php?pid=59067942450599627'></script><!-- End: Star-Clicks.com -->    </div>
+<!--Begin: Star-Clicks.com HTML Code--><script type='text/javascript' src='https://www.star-clicks.com/secure/ads.php?pid=59067942450599627'></script><!-- End: Star-Clicks.com -->
+   </div>
   );
 }
